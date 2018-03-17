2a0dbf39047d9dacd561c4072c209f1e4a4a5b57
Xiaolin Zhang <leoncamel@gmail.com>
2018-03-17 19:28:53 +0800

    Clean-up useless testcases

--- tests/test_module.py
+++
@@ -1,6 +0,0 @@
-
-from datacanvas.module import *
-
-
-def test_case1():
-    assert True
--- tests/test_runtime.py
+++
@@ -1,6 +0,0 @@
-
-def test_EmrJarRuntime():
-    pass
-
-if __name__ == "__main__":
-    test_EmrJarRuntime()
